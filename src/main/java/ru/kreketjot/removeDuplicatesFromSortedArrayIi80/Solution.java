10bcbb247c4d3b967a11b55a2b0122281b189bdb
Andrey Vinokurov <kreketjot@gmail.com>
2025-06-28 16:38:18 +0300

    80. Remove Duplicates from Sorted Array II

---
+++ src/main/java/ru/kreketjot/removeDuplicatesFromSortedArrayIi80/Solution.java
@@ -0,0 +1,22 @@
+package ru.kreketjot.removeDuplicatesFromSortedArrayIi80;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        final int AT_MOST_COUNT = 2;
+        int lastValue = nums[0];
+        int remainCount = AT_MOST_COUNT - 1;
+        int k = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != lastValue) {
+                nums[k++] = nums[i];
+                lastValue = nums[i];
+                remainCount = AT_MOST_COUNT - 1;
+            } else if (remainCount > 0) {
+                nums[k++] = nums[i];
+                remainCount--;
+            }
+        }
+        return k;
+    }
+}
