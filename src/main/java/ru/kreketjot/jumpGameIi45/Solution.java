8407ec49a18430a4ba96b15998f1f70f42493a89
Andrey Vinokurov <kreketjot@gmail.com>
2025-07-02 18:43:31 +0300

    45. Jump Game II

---
+++ src/main/java/ru/kreketjot/jumpGameIi45/Solution.java
@@ -0,0 +1,21 @@
+package ru.kreketjot.jumpGameIi45;
+
+public class Solution {
+    public int jump(int[] nums) {
+        int jumpsCount = 0;
+        int maxI = 0;
+        int jumpOnI = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxI = Math.max(maxI, nums[i] + i);
+            if (maxI >= nums.length - 1) {
+                jumpsCount++;
+                break;
+            }
+            if (i == jumpOnI) {
+                jumpsCount++;
+                jumpOnI = maxI;
+            }
+        }
+        return jumpsCount;
+    }
+}
